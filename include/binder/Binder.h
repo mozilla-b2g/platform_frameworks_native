82e23190b8845fa74f191a467fd67204bc2de093
novski <i.hoerler@me.com>
2016-09-06 22:36:51 +0200

    Bug 1294725 - Fairphone 2 needs special caf repo
    Atomic C++ was missing see commit
    e92af361af5155574c3f05b9544aef194b760aa0
    for mor information

--- include/binder/Binder.h
+++ include/binder/Binder.h
@@ -14,10 +14,17 @@
  * limitations under the License.
  */
 
+
 #ifndef ANDROID_BINDER_H
 #define ANDROID_BINDER_H
 
+#if __cplusplus >= 201103L
 #include <atomic>
+typedef std::atomic<uintptr_t> atomic_uintptr_t;
+#else
+#include <stdatomic.h>
+#endif
+
 #include <stdint.h>
 #include <binder/IBinder.h>
 
